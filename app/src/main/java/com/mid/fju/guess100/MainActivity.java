a6aa8197a410a0be0ed843cd6dfb90c2f9fd5e2b
nizzi0623 <julie870529@gmail.com>
2017-11-14 10:49:27 +0800

    Guess

--- app/src/main/java/com/mid/fju/guess100/MainActivity.java
+++ app/src/main/java/com/mid/fju/guess100/MainActivity.java
@@ -1,13 +1,21 @@
 package com.mid.fju.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
+    int r =random.nextInt(99)+1;
+    int min = 1;
+    int max= 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +27,30 @@ public class MainActivity extends AppCompatActivity {
     public void findViews () {
         TextView secret = (TextView) findViewById(R.id.secret);
         TextView info = (TextView) findViewById(R.id.info);
-        EditText number = (EditText) findViewById(R.id.number);
+        EditText edNum = (EditText) findViewById(R.id.number);
         Button send = (Button) findViewById(R.id.send);
+        int n = Integer.parseInt(edNum.getText().toString());
+
+        while (n<r && n>min && n<max){
+            new AlertDialog.Builder(this).setMessage(n + " to " + r).setPositiveButton("OK", null).show();
+            break;
+        }
+        while (n>r && n>min && n<max){
+            new AlertDialog.Builder(this).setMessage(min + " to " + n).setPositiveButton("OK", null).show();
+            break;
+        }
+        while (n==r){
+            new AlertDialog.Builder(this).setMessage("答對了").setPositiveButton("OK", null).show();
+            break;
+        }
+
+
+
+
+
+
+
+
 
 
     }
